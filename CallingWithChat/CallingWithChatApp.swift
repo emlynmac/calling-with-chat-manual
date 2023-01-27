17c9849c6d217dae669a88214449e46e8906c23e
Emlyn Bolton <3941071+emlynmac@users.noreply.github.com>
2023-01-27 14:22:36 -0800

    Initial Commit

---
+++ CallingWithChat/CallingWithChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  CallingWithChatApp.swift
+//  CallingWithChat
+//
+//  Created by Emlyn Bolton-Maggs on 2023-01-27.
+//
+
+import SwiftUI
+
+@main
+struct CallingWithChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
